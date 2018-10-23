c0e494f15e90e8dabe88eadcee01988d5083173b
qianbingjian <2167325279@qq.com>
2018-10-23 15:23:30 +0800

    'promise'

---
+++ promise.js
@@ -0,0 +1,16 @@
+/**
+ * @desc promise解决回调问题 promise.all().then() 所有的promise完成，promise.race().then()只要有 一个完成
+ */
+let a = 11;
+let promise = new Promise((resolve, reject)=>{
+    if (Object.is(a,10)){
+        resolve('成功')
+    } else {
+        reject('失败...')
+    }
+})
+promise.then( res => {
+    console.log(res)
+}).catch(err=>{
+    console.log(err)
+})
